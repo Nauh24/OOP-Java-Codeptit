b6914f2da15ce0f857be510cb8cc5fefd738429f
Nauh24 <nvnauh@gmail.com>
2023-09-04 22:58:27 +0700

    4/9/23

---
+++ J04001_KhaiBaoLopPoint/Point.java
@@ -0,0 +1,31 @@
+package J04001_KhaiBaoLopPoint;
+
+public class Point {
+
+    private double x, y;
+
+    Point() {
+        this.x=0;
+        this.y=0;
+    }
+    public Point(double x,double y){
+        this.x=x;
+        this.y=y;
+    }
+    Point(Point p){
+        this.x=p.x;
+        this.y=p.y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    double distance(Point p){
+        return Math.sqrt(pow)
+    (
+    }
+}
